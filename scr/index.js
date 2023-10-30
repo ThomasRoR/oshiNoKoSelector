4610ede656f3d08bfc21c1fb1a371deb69891b14
ThomasRoR <thomasraul06@gmail.com>
2023-10-30 11:12:07 -0300

    feat: add javascript functional selector, and data's in html to import

--- scr/index.js
+++ scr/index.js
@@ -1,1 +1,26 @@
 const personagens = document.querySelectorAll('.personagem')
+ 
+personagens.forEach(personagem => {
+    personagem.addEventListener('mouseenter', () => {
+
+        if(window.innerWidth < 450){
+            window.scrollTo({top : 0, behavior: 'smooth'})
+        }
+
+        const personagemSeleciondo = document.querySelector('.selecionado')
+        personagemSeleciondo.classList.remove('selecionado')
+
+        personagem.classList.add('selecionado');
+
+        const imagemPersonagemGrande = document.querySelector('.personagemGrande')
+        const idPersonagem = personagem.attributes.id.value;
+        imagemPersonagemGrande.src = "/scr/card/card-" + (idPersonagem) + ".png";
+
+        const nomePersonagem = document.getElementById('nome-personagem');
+        nomePersonagem.innerText = personagem.getAttribute('data-name');
+        
+        const descricaoPersonagem = document.getElementById('descricao-personagem');
+        descricaoPersonagem.innerText = personagem.getAttribute('data-description');
+
+    })
+})
